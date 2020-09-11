75381780f5e2e2597ec4def11e8e16b922ef758e
Felipe <fb.dc.col@gmail.com>
2020-09-11 12:02:52 -0500

    update

---
+++ src/components/BookItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const BookItem = styled.section`
+
+    h2 {
+        small {
+            font-weight: normal;
+        }
+    }
+`
