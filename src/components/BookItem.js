77a1853d87abce83006dc3ff2ce90687009ade04
Felipe <fb.dc.col@gmail.com>
2020-09-11 15:00:42 -0500

    update

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,11 +1,28 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react"
+import styled from "styled-components"
 
-export const BookItem = styled.section`
-
-    h2 {
-        small {
-            font-weight: normal;
-        }
+const BookItemWrapper = styled.section`
+  background: white;
+  border: 1px solid #ddd;
+  padding: 8px;
+  margin-bottom: 8px;
+  h2 {
+    small {
+      font-weight: normal;
+      padding-left: 8px;   
+      font-size: 14px;
     }
+  }
 `
+
+export const BookItem = ({ title, description, year, children }) => {
+  return (
+    <BookItemWrapper>
+      <h2>
+        {title} - <small>{year}</small>
+      </h2>
+      <p>{description}</p>
+      <div>{children}</div>
+    </BookItemWrapper>
+  )
+}
