0590c4e6b09b0a4f19721adce3e1582017e4e26e
Felipe <fb.dc.col@gmail.com>
2020-09-13 09:19:14 -0500

    update

--- src/components/header.js
+++ src/components/header.js
@@ -1,49 +1,107 @@
-import { Link } from "gatsby"
+import React, { useContext } from "react"
+import { Link, navigate } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
 import styled from "styled-components"
-import {FirebaseContext} from './Firebase'
-  
+import { FirebaseContext } from "./Firebase"
+
+const LogoutLink = styled.span`
+  color: white;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+const Divider = styled.span`
+  margin: 0 8px;
+  padding-right: 1px;
+  background: #ddd;
+`
+
 const StyledHeader = styled.header`
   margin-bottom: 1.45rem;
   background-position: center center;
   background: url(https://user-images.githubusercontent.com/15071636/92960335-0e617980-f433-11ea-9aa3-09e5f8d81a28.jpg)
     no-repeat center/cover;
-  text-align: right;
+  text-align: left;
   opacity: 1.2;
   padding: 40px;
 `
+const HeaderContent = styled.header`
+  margin: 0 auto;
+  max-width: 960;
+  padding: 1.45rem 1.0875rem;
+  display: flex;
+
+  >h1 {
+    margin: 0;
+    flex-grow: 1;
+
+    >a {
+      color: white;
+      text-decoration: none;
+    }
+
+  }
+  >div{
+    margin: auto 0; 
+  }
+`
+
+const UserInfo = styled.div`
+  text-align: right;
+  color: white;
+
+`
+
+const LoginLink = styled.div`
+  margin: auto 0;
+  a {
+    text-decoration: none;
+    color: white;
+  }
+`
 
 const Header = ({ siteTitle }) => {
+  // most convenient way to access firebase context for our needs
+  const { firebase, user } = useContext(FirebaseContext)
+
+  function handleLogoutClick() {
+    firebase.logout().then(() => {
+      navigate("/login")
+    })
+  }
+
   return (
     <StyledHeader>
-      <div
-        style={{
-          margin: `0 au to`,
-          maxWidth: 960,
-          padding: `1.45rem 1.0875rem`,
-        }}
-      >
-        <h1 style={{ margin: 0 }}>
+      <HeaderContent>
+        <h1>
           <Link
             to="/"
-            style={{
-              color: `white`,
-              textDecoration: `none`,
-            }}
           >
             {siteTitle}
           </Link>
         </h1>
-        <FirebaseContext.Consumer>
-          {
-            props => { 
-              console.log(props)
-              return <div></div>
-            }
-          }
-        </FirebaseContext.Consumer>
-      </div>
+        <div>
+          {!!user && !!user.email && (
+            <UserInfo>
+              Hola, {user.email}
+              <div style={{ textAlign: "right" }}>
+                <LogoutLink onClick={handleLogoutClick}>Logout</LogoutLink>
+              </div>
+            </UserInfo>
+          )}
+        </div>
+        <LoginLink >
+          {(!user || !user.email) && (
+            <div>
+              <Link to="/login">Login</Link>
+              <Divider />
+              <Link to="/signup">Signup</Link>
+            </div>
+          )}
+        </LoginLink>
+      </HeaderContent>
     </StyledHeader>
   )
 }
