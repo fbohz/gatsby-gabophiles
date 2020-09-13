6600eb5f152bd49d9cac8192729e8287f14597bc
Felipe <fb.dc.col@gmail.com>
2020-09-13 09:55:28 -0500

    update

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,11 +1,12 @@
 import React, {useState, useContext} from "react"
 import styled from "styled-components"
 import {FirebaseContext} from '../components/Firebase'
-
+import { navigate } from "gatsby"
 import {Form, Input, Button} from "../components/common/index"
 
 const Signup = () => {
     const {firebase} = useContext(FirebaseContext)
+    const [error, setError] = useState('')
 
     const [formValues, setFormValues] = useState({
         email: '',
@@ -22,7 +23,10 @@ const Signup = () => {
                   password: formValues.password
               }).then(() => {
                 navigate("/")
-              })
+              }).catch(e => {
+                console.log(e)
+                setError(e.message)
+            })
         }
 
         console.log(formValues)
@@ -42,6 +46,9 @@ const Signup = () => {
             <Input onChange={handleChange} value={formValues.email} name="email" placeholder="email" type="email" required />
             <Input onChange={handleChange} value={formValues.password} name="password" placeholder="password" type="password" minLength={3} required />
             <Input onChange={handleChange} value={formValues.confirmPassword} name="confirmPassword" placeholder="confirm password" type="password" minLength={3} required />
+            {
+            !!error && <span>{error}</span>
+            }
             <Button type="submit">Signup</Button>
         </Form>
     )
