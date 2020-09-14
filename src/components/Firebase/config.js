52e100e4e2b554a0496dd966a2397caf4cd8dac7
Felipe <fb.dc.col@gmail.com>
2020-09-14 10:34:24 -0500

    update

--- src/components/Firebase/config.js
+++ src/components/Firebase/config.js
@@ -1,12 +1,16 @@
+// On development these must be saved to env.development! not on just .env! On production ensure to save to appropriate app.
+
+// also read: https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public
+
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
+    apiKey: process.env.FIREBASE_apiKey,
+    authDomain: process.env.FIREBASE_authDomain,
+    databaseURL: process.env.FIREBASE_databaseURL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_storageBucket,
+    messagingSenderId: process.env.FIREBASE_messagingSenderId,
+    appId: process.env.FIREBASE_appId,
+    measurementId: process.env.FIREBASE_measurementId
   };
 
   export default firebaseConfig
